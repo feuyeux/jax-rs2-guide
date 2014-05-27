626412141c8b0ff0a2f9b577014e92f5c601c518
Eric Han <feuyeux@gmail.com>
2014-05-27 15:46:03 +0800

    upgrade to jersey2.9

--- upgrade.jersey.sh
+++ upgrade.jersey.sh
@@ -1,2 +1,1 @@
-sed -i "s/jersey.version>2.6/jersey.version>2.9/g" `grep 'ersey.version>2.7' -rl ./sample`
-
+sed -i "" 's/jersey.version>2.[0-9]/jersey.version>2.9/g' `grep 'jersey.version>' -rl ./sample`
